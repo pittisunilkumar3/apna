799c774222a77780d0eb58a30308a9f74120c92d
pittisunilkumar3 <pittisunilkumar3@gmail.com>
2025-10-16 23:32:16 +0530

    first commit

---
+++ lib/views/screens/onbording/onbording_data.dart
@@ -0,0 +1,33 @@
+import 'package:listplace/utils/app_constants.dart';
+
+class OnBordingData {
+  String imagePath;
+  String title;
+  String description;
+
+  OnBordingData(
+      {required this.imagePath,
+      required this.title,
+      required this.description});
+}
+
+List<OnBordingData> onBordingDataList = [
+  OnBordingData(
+      imagePath: "$rootImageDir/onbording_1.png",
+      title: "WELCOME TO APNA",
+      description:
+          "Explore a wide variety of categories and discover businesses, services, and places tailored to your needs."),
+  OnBordingData(
+      imagePath: "$rootImageDir/onbording_2.png",
+      title: "Select Your Price Schedule",
+      description:
+          "Compare prices and options at a glance to choose the listings that best fit your budget and preferences."),
+  OnBordingData(
+      imagePath: "$rootImageDir/onbording_3.png",
+      title: "WELCOME TO APNA",
+      description:
+          "Easily locate top-rated places in your area and enjoy hassle-free access to all the information you need."),
+];
+
+
+
